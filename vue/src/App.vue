e0df293df49172d88030926fbd46b449ae1d28e9
Denis Flenchea <denslrrwork@gmail.com>
2024-06-27 00:21:51 +0300

    vue3

---
+++ vue/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+     <input v-model="userName" type="text" placeholder="Name">
+     <input v-model="userPassword" type="password" placeholder="Password">
+     <input v-model="userEmail" type="email" placeholder="Email">
+
+    <h2>Мое имя:{{userName}}</h2>
+    <h2>Мой пароль:{{userPassword}}</h2>
+    <h2>Мой маил:{{userEmail}}</h2>
+
+</template>
+
+
+
+
+<script >
+import Prime from './components/Prime.vue'
+ export default {
+    data () {
+      return {
+          userName: '',
+          userPassword: '',
+          userEmail: '',
+      }
+    },
+    methods: {
+      
+
+    }
+ }
+
+
+</script>
+
+
+
+<style scoped>
+  h3  { 
+    font-weight: bold;
+  }
+  p {
+
+    color: red;
+  }
+
+</style>
