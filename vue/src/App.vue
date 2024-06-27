9d961e9f521cf496875805fd94c5431f42a2a53e
Denis <denslrrwork@gmail.com>
2024-06-27 13:20:29 +0300

    use props and adding delete function

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,28 +2,75 @@
      <input v-model="userName" type="text" placeholder="Name">
      <input v-model="userPassword" type="password" placeholder="Password">
      <input v-model="userEmail" type="email" placeholder="Email">
+     <button @click="sendData">Send</button>
+     <p className="error">{{error}}</p>
 
-    <h2>Мое имя:{{userName}}</h2>
-    <h2>Мой пароль:{{userPassword}}</h2>
-    <h2>Мой маил:{{userEmail}}</h2>
 
+    <div v-if="users.length == 0">
+      <h1>Not users</h1>
+    </div>
+
+    <User 
+    v-for="(el, index) in users" 
+    :key="index" :user="el" 
+    class="box"
+    :index="index"
+    :deleteUser="deleteUser"
+    />
+
+    
+    
 </template>
 
 
 
 
 <script >
-import Prime from './components/Prime.vue'
+import User from './components/User.vue'
  export default {
+  components: {
+    User,
+  },
+
+
+
     data () {
       return {
-          userName: '',
-          userPassword: '',
-          userEmail: '',
+        users: [],
+        error: '',
+        userName: '',
+        userPassword: '',
+        userEmail: '',
+          
       }
     },
     methods: {
-      
+      sendData() {
+
+        if (this.userName == '') {
+            this.error = 'Имя не введено'
+            return;
+        } else if (this.userEmail == '') {
+          this.error = 'Email не введен'
+          return;
+        } else if (this.userPassword == '') {
+          this.error = 'Пароль не введен'
+          return;
+        }
+
+        this.error = '';
+
+        this.users.push({
+          name: this.userName,
+          pass: this.userPassword,
+          email: this.userEmail,
+        })
+      },
+
+      deleteUser(index) {
+        this.users.splice(index, 1)
+      }
+
 
     }
  }
@@ -33,13 +80,23 @@ import Prime from './components/Prime.vue'
 
 
 
-<style scoped>
+<style >
   h3  { 
     font-weight: bold;
   }
   p {
 
+    color: black;
+  }
+  .pone {
     color: red;
   }
+  .box {
+    width: 250px;
+    height: 180px;
+    border: 1px solid black;
+    border-radius: 5px;
+  }
 
+  
 </style>
---
+++ vue/src/components/User.vue
@@ -0,0 +1,45 @@
+<template>
+  
+    <div class="block">
+          <p class="pone">{{user.name}}</p>
+          <p class="pone">{{user.pass}}</p>
+          <p class="pone">{{user.email}}</p>
+
+          <button @click="deleteUser(index)">DELETE</button>
+    </div>
+  
+</template>
+
+<script>
+  export default {
+    props: {
+      user: {
+        type: Object,
+        required: true,
+      },
+      index: {
+        type: Number,
+        required: true,
+      },
+      deleteUser: {
+        type: Function,
+        required: true,
+      }
+    }
+  }
+</script>
+
+<style  scoped>
+  .block {
+    background-color: rgba(128, 128, 128, 0.349);
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+
+  p {
+    color: black;
+  }
+</style>
